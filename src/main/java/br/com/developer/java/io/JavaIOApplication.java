faa052b20cf0f7166d1df8a4da57711433c99b33
Unknown <luizcesarmais44@hotmail.com>
2019-02-22 00:48:59 -0300

    Commit inicial

---
+++ src/main/java/br/com/developer/java/io/JavaIOApplication.java
@@ -0,0 +1,13 @@
+package br.com.developer.java.io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaIOApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaIOApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/developer/java/io/controller/FileController.java
@@ -0,0 +1,8 @@
+package br.com.developer.java.io.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FileController { 
+
+}
