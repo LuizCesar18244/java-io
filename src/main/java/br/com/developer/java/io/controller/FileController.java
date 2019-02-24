cbf0f48b7814342bf9f5eba85e11d6d904a7c066
Unknown <luizcesarmais44@hotmail.com>
2019-02-24 19:49:42 -0300

    CrossOrigin e RequestMapping.

--- src/main/java/br/com/developer/java/io/controller/FileController.java
+++ src/main/java/br/com/developer/java/io/controller/FileController.java
@@ -1,8 +1,20 @@
 package br.com.developer.java.io.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
+@RequestMapping( "/api" )
 public class FileController { 
+	
+	@RequestMapping( value = "/file",method = RequestMethod.POST)
+	public @ResponseBody String teste() 
+	{
+		return "Arquivo enviado...";
+	}
 
 }
